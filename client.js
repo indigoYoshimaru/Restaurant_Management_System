5d79de28061bbc418f5d72c64903cb187b4b5c41
KazehaKhoa <62781930+KazehaKhoa@users.noreply.github.com>
2020-05-21 13:29:23 +0700

    Add files via upload
    update manager

--- client.js
+++ client.js
@@ -12,103 +12,111 @@ async function getJson(testRoute, param) {
     })).json();
 
     console.log(r);
+    return r;
 }
 
 //=================================TABLE=============================//
+async function getMenuItemsByComboId(comboId) {
+    let data = await getJson('/api/general/get-menu-items-by-combo-id', comboId);
+    if (data.error)
+        throw new Error(data.error);
+    return data.result;
+}
+
+/************************** */
+
 async function getCurrentTableInfo() {
     //let text = await testPost('/api/get-current-table-info', {});
-    let data = await getJson('/api/table/info', {});
+    let data = await (await fetch('/api/table/info')).json();
 
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
-/***************************/
+/************************** */
 async function setUpTable(tableId) {
-    let data = await getJson('api/table/setup', { tableId });
+    let data = await getJson('/api/table/setup', { tableId });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
-/***************************/
+/************************** */
 async function orderItems(menuItemIds, comboIds) {
-    let data = await getJson('api/table/order', { menuItemIds, comboIds });
+    let data = await getJson('/api/table/order', { menuItemIds, comboIds });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
-/***************************/
+/************************** */
 async function checkPayment(cardNo, cvv) {
-    let data = await getJson('api/table/payment', { cardNo, cvv });
+    let data = await getJson('/api/table/payment', { cardNo, cvv });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
-
-/*****************************/
-async function addTable() {
-    let data = await getJson('api/manager/table/add', { Code, ServedState, PayState });
-    if (data.error)
-        throw new Error(data.error);
-    return data.result;
-}
-
-/*******************************/
-async function updateTable() {
-    let data = await getJson('api/manager/table/update', { tableId, Code, ServedState, PayState });
-    if (data.error)
-        throw new Error(data.error);
-    return data.result;
-}
-
 
 //=================================COOK=============================//
-/***************************/
+/************************** */
 async function getCurrentCookInfo() {
-    let data = await getJson('api/cook/info', {});
+    let data = await getJson('/api/cook/info', {});
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
-/***************************/
+/************************** */
 async function setUpCook(cookId) {
-    let data = await getJson('api/cook/setup', { cookId });
+    let data = await getJson('/api/cook/setup', { cookId });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
-/***************************/
+/************************** */
 async function startCooking(time) {
-    let data = await getJson('api/cook/start', { time });// the predicted cooking time in minute
+    let data = await getJson('/api/cook/start', { time });// the predicted cooking time in minute
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
-/***************************/
+/************************** */
 async function finishCooking() {
-    let data = await getJson('api/cook/finish', {});
+    let data = await getJson('/api/cook/finish', {});
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
-/*****************************/
-async function addCook() {
-    let data = await getJson('api/manager/cook/add', { personalId, firstName, lastName });
+//=================================MANAGER=============================//
+/*************************** */
+async function addCook(personalId, firstName, lastName) {
+    let data = await getJson('/api/manager/cook/add', { personalId, firstName, lastName });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
-/*****************************/
-async function updateCook() {
-    let data = await getJson('api/manager/cook/update', { cookId, personalId, firstName, lastName });
+/**************************** */
+async function addTable(code) {
+    let data = await getJson('/api/manager/table/add', { code });
     if (data.error)
         throw new Error(data.error);
     return data.result;
 }
 
+/**************************** */
+async function addItem(Name, Price, DefaultDuration, Available) {
+    let data = await getJson('/api/manager/item/add', { Name, Price, DefaultDuration, Available });
+    if (data.error)
+        throw new Error(data.error);
+    return data.result;
+}
 
+/**************************** */
+async function addCombo(Name, Price, Available) {
+    let data = await getJson('/api/manager/combo/add', { Name, Price, Available });
+    if (data.error)
+        throw new Error(data.error);
+    return data.result;
+}
